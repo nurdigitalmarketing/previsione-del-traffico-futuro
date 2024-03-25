892b3982ea26b1fcadfa55164d7a6be7e7679b74
NUR® Digital Marketing <163868030+nurdigitalmarketing@users.noreply.github.com>
2024-03-25 16:01:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly
-from datetime import datetime, timedelta
+from datetime import datetime
 import numpy as np
 
 def formatta_numero(numero):
@@ -14,13 +13,8 @@ def formatta_numero(numero):
     Returns:
         str: Il numero formattato con il punto come separatore delle migliaia.
     """
-    num_str = f"{numero:,}"  # Formatta il numero con la virgola come separatore delle migliaia
-    return num_str.replace(',', '.')  # Sostituisce la virgola con il punto
-
-# Esempio di utilizzo della funzione
-numero_formattato = formatta_numero(1234567)
-print(numero_formattato)  # Output: 1.234.567
-
+    num_str = f"{numero:,}"
+    return num_str.replace(',', '.')
 
 st.title('Previsione del Traffico Futuro')
 
@@ -28,11 +22,11 @@ st.markdown(
 """
 ## Introduzione
 
-Questo strumento è stato sviluppato per fornire previsioni sul traffico futuro basandosi sull'export dei dati degli utenti da _Google Analytics_ o sui dati di ricerca organica da _Ahrefs_ o _Semrush_. Attraverso l'utilizzo di modelli di previsione avanzati, facilita la comprensione delle tendenze future basate sui dati storici.
+Questo strumento è stato sviluppato per fornire previsioni sul traffico futuro basandosi sull'export dei dati degli utenti da Google Analytics o sui dati di ricerca organica da Ahrefs/Semrush. Attraverso l'utilizzo di modelli di previsione avanzati, facilita la comprensione delle tendenze future basate sui dati storici.
 
 ## Funzionamento
 
-Per garantire previsioni accurate, segui i passaggi dettagliati relativi all'origine dei tuoi dati. Ecco come preparare i dati esportati da Google Analytics, Ahrefs o Semrush.
+Per garantire previsioni accurate, segui i passaggi dettagliati relativi all'origine dei tuoi dati. Ecco come preparare i dati esportati da Google Analytics e Ahrefs/Semrush.
 """)
 
 with st.expander("Da Google Analytics"):
@@ -44,9 +38,9 @@ with st.expander("Da Google Analytics"):
        - Esporta i dati nel formato CSV.
 
     2. **Pulizia dei dati:**
-       - Apri il file CSV su Google Fogli.
-       - Elimina tutte le righe dalla 1 alla 8 e dalla riga dopo l'ultima "Ennesima settimana" del primo blocco di data e utenti.
-       - Fatto ciò, assicurati che le colonne siano nominate correttamente: la colonna con le date deve essere rinominata in `Date` e la colonna con i volumi di traffico in `Organic Traffic`.
+       - Apri il file CSV con un editor di fogli di calcolo (es. Excel, Google Sheets).
+       - Assicurati che le colonne siano nominate correttamente: la colonna con le date deve essere rinominata in `Date` e la colonna con i volumi di traffico in `Organic Traffic`.
+       - Elimina eventuali righe o colonne non necessarie che non contengono dati relativi al traffico o alle date.
 
         _Qui puoi trovare un [esempio](https://drive.google.com/file/d/1v4ZpiG8Kijwn1uRm02S1yMRQkWH1G4ov/view?usp=sharing) di come dovrebbe apparire._
 
@@ -67,34 +61,11 @@ with st.expander("Da Ahrefs"):
        - Seleziona il periodo di tempo desiderato e esporta i dati relativi al traffico di ricerca organica.
 
     2. **Pulizia dei dati:**
-       - Apri il file esportato con Google Fogli.
-       - Rinomina la colonna con le date in `Date` e quella con i volumi di traffico in `Organic Traffic`.
+       - Apri il file esportato con un software di fogli di calcolo.
+       - Rinomina la colonna con le date in `Date` e quella con i volumi di traffico in `Traffic`.
        - Rimuovi le righe e le colonne non pertinenti che non contengono dati di traffico o date.
 
        _Qui puoi trovare un [esempio](https://drive.google.com/file/d/1v4cqG_v8b85t9A7OImsAINKGUCh_eRey/view?usp=sharing) di come dovrebbe apparire._
-
-    3. **Caricamento del file:**
-       - Carica il file CSV pulito attraverso l'interfaccia di caricamento fornita dallo strumento.
-    """
-    )
-
-with st.expander("Da Semrush"):
-    st.markdown(
-    """
-    1. **Esportazione dei dati:**
-       - Accedi a Semrush e vai alla sezione di _panoramica dominio_ per il tuo sito.
-       - Seleziona il periodo di tempo desiderato (i.e. 2A) e esporta i dati relativi al traffico di ricerca organica.
-
-    2. **Pulizia dei dati:**
-       - Apri il file esportato con Google Fogli.
-       - Rimuovi le colonne A, B, C e D e le colonne dalla 3 alla 9 comprese.
-       - Rinomina la colonna `Summary` in `Date` e la cella sotto `Summary` in `Organic Traffic`.
-       - Esporta il file in CSV ed importalo nello strumento [CSV rows and columns converter](https://onlinecsvtools.com/convert-csv-rows-to-columns) cliccando su _import from file_ sotto _input csv_.
-       - A questo punto, sotto _output csv_, copia il risultato ottenuto.
-       - Apri un nuovo foglio su Google Fogli e incolla il risultato ottenuto. Vai su `Dati` e clicca su `Suddividi il testo in colonne`.
-       - Esporta il file in formato CSV.
-
-       _Qui puoi trovare un [esempio](https://drive.google.com/file/d/1ZkfuqbHcxQhm5zX8L_nKf0OPWAYTg3Hr/view?usp=sharing) di come dovrebbe apparire._
 
     3. **Caricamento del file:**
        - Carica il file CSV pulito attraverso l'interfaccia di caricamento fornita dallo strumento.
@@ -114,76 +85,43 @@ if origine_dati == 'Google Analytics':
 # Caricamento del file CSV
 uploaded_file = st.file_uploader("Carica il file CSV del traffico", type="csv")
 if uploaded_file is not None:
-    if origine_dati == 'Google Analytics':
-        traffic = pd.read_csv(uploaded_file)
-        start_date = datetime.strptime(data_inizio, '%Y%m%d')
-        traffic['ds'] = traffic['Date'].apply(lambda x: start_date + timedelta(weeks=int(x)))
-        traffic.rename(columns={'Organic Traffic': 'y'}, inplace=True)
-    elif origine_dati == 'Ahrefs':
-        traffic = pd.read_csv(uploaded_file)
-        if 'Date' not in traffic.columns:
-            st.error("Assicurati che il file CSV abbia la colonna 'Date' nel formato 'YYYY-MM-DD'.")
-        else:
-            traffic.rename(columns={'Date': 'ds', 'Organic Traffic': 'y'}, inplace=True)
-            traffic['ds'] = pd.to_datetime(traffic['ds'])
-    elif origine_dati == 'Semrush':  # Gestione del caso Semrush
-        traffic = pd.read_csv(uploaded_file)
-        # Assicurati che il file CSV abbia una colonna 'Date' nel formato 'YYYY-MM'
-        if 'Date' not in traffic.columns or 'Organic Traffic' not in traffic.columns:
-            st.error("Assicurati che il file CSV abbia le colonne 'Date' nel formato 'YYYY-MM' e 'Organic Traffic'.")
-        else:
-            # Conversione del formato della data da 'YYYY-MM' a datetime, aggiungendo '-01' per completare la data
-            traffic['ds'] = pd.to_datetime(traffic['Date'] + '-01')
-            traffic.rename(columns={'Organic Traffic': 'y'}, inplace=True)
-
-    # Verifica delle colonne per tutti i casi
+    traffic = pd.read_csv(uploaded_file)
     if origine_dati in ['Google Analytics', 'Ahrefs', 'Semrush']:
+        traffic['ds'] = pd.to_datetime(traffic['ds'])
+        traffic.rename(columns={'Organic Traffic': 'y'}, inplace=True)
+
+        # Verifica delle colonne
         if not {"ds", "y"}.issubset(traffic.columns):
             st.error("Assicurati che il DataFrame abbia le colonne 'ds' per la data e 'y' per la variabile target.")
         else:
-            updates = pd.DataFrame({
-              'holiday': 'Google Update',
-              'ds': pd.to_datetime(['2015-07-17', '2016-01-08',
-                                    '2016-09-27', '2017-03-08', '2017-07-09', '2018-03-08', '2018-04-17',
-                                    '2018-08-01', '2019-03-12', '2019-06-03', '2019-09-24', '2019-10-25',
-                                    '2019-12-09', '2020-01-13', '2020-05-04', '2020-12-03', '2021-06-02',
-                                    '2021-07-01', '2021-11-17', '2022-05-25', '2023-09-15', '2023-10-05',
-                                    '2023-11-02', '2023-11-08', '2024-03-05']),
-              'lower_window': 0,
-              'upper_window': 1,
-            })
-
-            m = Prophet(holidays=updates)
+            # Preparazione dei dati e addestramento del modello
+            m = Prophet()
             m.fit(traffic)
-            future = m.make_future_dataframe(periods=365)
+
+            # Calcolo della lunghezza del periodo di input
+            lunghezza_periodo_input = (traffic['ds'].max() - traffic['ds'].min()).days + 1
+            future = m.make_future_dataframe(periods=lunghezza_periodo_input)
             forecast = m.predict(future)
 
-            # Calcolo e visualizzazione dell'incremento previsto del traffico
-            inizio_previsioni = forecast['ds'].min()
-            fine_previsioni = forecast['ds'].max()
-            traffic_primo_mese = forecast[forecast['ds'] == inizio_previsioni]['yhat'].sum()
-            traffic_ultimo_mese = forecast[forecast['ds'] == fine_previsioni]['yhat'].sum()
-            incremento = traffic_ultimo_mese - traffic_primo_mese
-            percentuale_incremento = (incremento / traffic_primo_mese) * 100
+            # Calcolo dell'incremento del traffico
+            periodo_input = forecast[(forecast['ds'] >= traffic['ds'].min()) & (forecast['ds'] <= traffic['ds'].max())]
+            periodo_previsione = forecast[(forecast['ds'] > traffic['ds'].max()) & (forecast['ds'] <= traffic['ds'].max() + pd.Timedelta(days=lunghezza_periodo_input))]
+
+            traffico_totale_input = periodo_input['yhat'].sum()
+            traffico_totale_previsione = periodo_previsione['yhat'].sum()
+            incremento = traffico_totale_previsione - traffico_totale_input
+            percentuale_incremento = (incremento / traffico_totale_input) * 100
 
             st.info(f"""
                 **Stima dell'aumento del traffico con il metodo NUR®:**
-                - Si stima un aumento di traffico da {formatta_numero(int(traffic_primo_mese))} utenti nel primo mese a {formatta_numero(int(traffic_ultimo_mese))} utenti nell'ultimo mese del periodo di previsione.
+                - Incremento previsto: {formatta_numero(int(incremento))} utenti
                 - **Incremento percentuale:** {percentuale_incremento:.2f}%
             """)
-            
-            ## st.write("Anteprima dei dati caricati:")
-            ## st.write(traffic.head())
 
+            # Visualizzazione delle previsioni
             st.subheader("Previsioni del traffico futuro")
             fig1 = plot_plotly(m, forecast)
             st.plotly_chart(fig1)
             
-            ## st.subheader("Componenti del forecast")
-            ## fig2 = plot_components_plotly(m, forecast)
-            ## st.plotly_chart(fig2)
-            
-            st.download_button(label="Scarica le previsioni in formato CSV",
-                               data=forecast.to_csv().encode('utf-8'),
-                               file_name='previsioni_traffico_futuro.csv',
-                               mime='text/csv')
+            # Opzione per scaricare le previsioni
+            st.download_button(label="Scarica le previsioni in formato CSV", data=forecast.to_csv().encode('utf-8'), file_name='previsioni_traffico_futuro.csv', mime='text/csv')
